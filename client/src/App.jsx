d9c47e2bc5a4556475030d4290baf1b88fbfebc8
destineevaldivia <destvaldivia@gmail.com>
2023-09-18 17:12:36 -0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNavBar from './components/Navbar'
+import ListStudents from './components/ListStudents'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      {/* <h2>Hello its me and Beyza and Rachel and Cristina</h2> */}
+      <MyNavBar />
+      <ListStudents />
+
+    </div>
+  )
+}
+
+export default App
